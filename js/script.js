2a68318ae655ba118ae231cf09efbedf0cf6ca81
David Medeiros <123705989+dav-med@users.noreply.github.com>
2023-03-20 17:37:30 +0000

    Add operate() function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 function main() {
+  let x = 8;
+  let y = 2;
+  let operator = '/';
+  console.log(operate(x, y, operator));
   return 0; 
 }
 
@@ -17,3 +21,26 @@ function multiply(x, y) {
 function divide(x, y) {
   return x / y;
 }
+
+function operate(x, y, operator) {
+  let result = 0;
+  switch (operator) {
+    case '+':
+      result = sum(x, y);
+      break;
+    case '-':
+      result = subtract(x, y);
+      break;
+    case '*':
+      result = multiply(x, y);
+      break;
+    case '/':
+      result = divide(x, y);
+      break;
+    default:
+      return 'Invalid operator';
+  }
+  return result;
+}
+
+main();
