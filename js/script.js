29cc91a6a2b042c706e1806562d122e2e808f1d7
David Medeiros <123705989+dav-med@users.noreply.github.com>
2023-03-20 17:19:56 +0000

    Add basic math functions

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function main() {
+  return 0; 
+}
+
+function sum(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
