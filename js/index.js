bdef77d3e6d954800f6813e46228fb909dde68a2
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-07-24 16:54:53 +0100

    Create operate()
    This function takes in an operator and two numbers and returns a result.

--- js/index.js
+++ js/index.js
@@ -16,3 +16,27 @@ function divide(number1, number2) {
     }
     return number1 / number2;
 }
+
+function operate(operator, number1, number2) {
+    let result = 0;
+
+    switch (operator) {
+        case '+':
+            result = sum(number1, number2);
+            break;
+        case '-':
+            result = subtract(number1, number2);
+            break;
+        case '*':
+            result = multiply(number1, number2);
+            break;
+        case '/':
+            result = divide(number1, number2);
+            break;
+        default:
+            result = 'Invalid operator'
+            break;
+    }
+
+    return result;
+}
