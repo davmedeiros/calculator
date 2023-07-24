fa9fc5dfff90b502b2b24ae0641ec567922f560d
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-07-24 16:41:32 +0100

    Create common math functions
    add, subtract, multiply, divide

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function sum(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    if (y === 0) {
+        return 'You maybe on to something'
+    }
+    return x / y;
+}
